cedae4b9d33da47d80d8d2ce08940f8c33dd1403
loudias88 <luisheleno.dias@gmail.com>
2022-11-10 19:02:57 -0300

    add JS functions

---
+++ main.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const openModal = () => document.getElementById('modal')
+    .classList.add('active')
+
+const closeModal = () => document.getElementById('modal')
+    .classList.remove('active')
+
+const tempClient = {
+    nome: "Luis",
+    email: "luisheleno.dias@gmail.com",
+    celular: "51981335390",
+    cidade: "Novo Hamburgo"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
+
+// CRUD - create read update delete
+
+const UpdateClient = (index, client) => {
+    const dbClient = readClient()
+    dbClient[index] = client
+    setLocalStorage(dbClient)
+}
+
+const readClient = () => getLocalStorage()
+
+const createClient = (client) => {
+    const dbClient = getLocalStorage()
+    dbClient.push (client)
+    setLocalStorage(dbClient)
+}
+
+//Eventos
+
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', openModal)
+
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal)
